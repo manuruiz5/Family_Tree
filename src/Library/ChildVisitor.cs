4cfa0a25d5b7289292d67df6fd98674d0bbc1b1b
manuruiz5 <manuela.ruiz@correo.ucu.edu.uy>
2023-11-04 16:16:36 -0300

    parte child visitor con error

--- src/Library/ChildVisitor.cs
+++ src/Library/ChildVisitor.cs
@@ -1,12 +1,14 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System;
+using System.Text;
 
 namespace Library
 {
-    public class ChildVisitor : Visitor
+    public class ChildVisitor:Visitor
     {
-        public Person Child {get;private set;}
-        public int MaxAge {get; private set;}= int.MinValue;
+        public Person Child{get;private set;}
+        public int MaxAge{get;private set;}=int.MinValue;
 
         public override void Visit(Node node)
         {
@@ -15,14 +17,14 @@ namespace Library
                 child.Accept(this);
             }
         }
-
         public override void Visit(Person person)
         {
             if (person.Age > MaxAge)
             {
-                Child = person;
-                MaxAge = person.Age;
+                Child=person;
+                MaxAge=person.Age;
             }
         }
     }
-}
+} 
+
